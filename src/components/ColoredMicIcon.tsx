829921b9477de0f7bab47185ac0be0729ad03ce2
Md Shayaan khan <mohammedkhanshayaan@gmail.com>
2025-07-22 13:44:03 +0000

    set microphone color like this

---
+++ src/components/ColoredMicIcon.tsx
@@ -0,0 +1,26 @@
+const ColoredMicIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    className="h-5 w-5"
+  >
+    <path
+      fill="#4285F4"
+      d="M12 15a4 4 0 0 0 4-4V5a4 4 0 1 0-8 0v6a4 4 0 0 0 4 4Z"
+    ></path>
+    <path fill="#34A853" d="M11 18.9h2v6h-2z"></path>
+    <path
+      fill="#F4B400"
+      d="m18.4 12-.7-1.3a7 7 0 0 0-10.4 0l-.7 1.3a9 9 0 0 0 5.4 3.7V20h-3v2h8v-2h-3v-4.3c2.5-1 4.5-3 5.4-5.7Z"
+    ></path>
+    <path
+      fill="#EA4335"
+      d="M17 11a5 5 0 0 1-10 0V5a5 5 0 1 1 10 0v6Z"
+    ></path>
+  </svg>
+);
+
+export default ColoredMicIcon;
